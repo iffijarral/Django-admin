1191603f1f8c7d441b5cf13f88310a16c543a019
Iftikhar Ahmed <iffi_kunjahi@yahoo.com>
2022-11-12 19:45:00 +0100

    Update models.py

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -103,8 +103,8 @@ class Enrollment(models.Model):
     # Other fields and methods you would like to design
 class Question(models.Model):
     # Foreign key to lesson
-    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
-    question = models.CharField(max_length=300, null=False)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=300, null=False)
     grade = models.IntegerField()
 
     # <HINT> A sample model method to calculate if learner get the score of the question
@@ -125,7 +125,7 @@ class Question(models.Model):
     # Other fields and methods you would like to design
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    content = models.CharField(max_length=300)
+    choice_text = models.CharField(max_length=300)
     is_correct = models.BooleanField(default=False)
 # <HINT> The submission model
 # One enrollment could have multiple submission
